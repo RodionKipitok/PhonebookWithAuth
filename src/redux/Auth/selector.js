64899c14d4324cb331f8643bdc60eed83ce8e719
admin <admin@admins-MacBook-Air.local>
2024-04-29 09:02:03 -0700

    added dr selector.js

--- src/redux/Auth/selector.js
+++ src/redux/Auth/selector.js
@@ -12,3 +12,35 @@ const setAuthHeader = token => {
 const clearAuthHeader = () => {
   axios.defaults.headers.common.Authorization = '';
 };
+
+// POST @ /users/signup
+// body: {name,email,password}
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/signup', credentials);
+      // After successful registration ,add the token to the HHTP header
+      setAuthHeader(res.data.token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// POST @ /users/login
+// body:{email,password}
+
+export const logIn = createAsyncThunk(
+  'user/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/user/login', credentials);
+      // After successful login , add the token to the HTTP header
+      setAuthHeader(res.data.token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
